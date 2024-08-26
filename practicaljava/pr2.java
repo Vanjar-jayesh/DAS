28173223d2c38af206e06bcf4b5ec743016b5fbf
vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>
2024-08-26 19:50:26 +0530

    Update pr2.java
    Signed-off-by: vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>

--- practicaljava/pr2.java
+++ practicaljava/pr2.java
@@ -1,5 +1,21 @@
 package practicaljava;
 
 public class pr2 {
+ public static void main(String[] args) {
+        int n ;
+        int fact = 1;
+
+        System.out.println("enter the number of n : ");
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+
+        for(int i = n; i>=1; i--){
+            fact =  fact* i;
+
+        }
+
+        System.out.println(fact);
+        // System.out.println(fact);
+    }
     
 }
