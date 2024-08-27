7463009e3a0ece1a951aae5cc291e63019d83c9e
vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>
2024-08-27 20:44:08 +0530

    Add files via upload
    Signed-off-by: vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>

---
+++ practicaljava/pr8.java
@@ -0,0 +1,33 @@
+package practicaljava;
+
+import java.util.Scanner;
+
+import Stack01.basic01;
+
+public class pr8 {
+    public static void main(String[] args) {
+      
+        int n;
+        System.out.println("enter the number : ");
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+
+        boolean isprime = true;
+
+        for (int i = 2; i <=n/2; i++) {
+            
+            if (n%i==0) {
+                isprime = false;
+                break;
+                
+            }
+        }
+        if (isprime) {
+            System.out.println(n+"is a prime number");
+        }
+        else{
+            System.out.println("not the prime number "+n);
+        }
+    }
+    
+}
