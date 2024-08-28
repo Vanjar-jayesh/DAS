7323a75e9ad6b84426bbcc583703969cdc0c515c
vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>
2024-08-28 20:54:23 +0530

    Add files via upload
    Signed-off-by: vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>

---
+++ practicaljava/pr12.java
@@ -0,0 +1,13 @@
+package practicaljava;
+
+public class pr12 {
+    public static void main(String[] args) {
+        
+        int [] arr = {2,3,1,4,5};
+
+        for (int i = 0; i <=arr.length; i++) {
+            
+            
+        }
+    }
+}
