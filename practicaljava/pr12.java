71672aa9f100b2deb6596cf52a08633690d07ebe
vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>
2024-09-10 15:23:22 +0530

    Update pr12.java
    Signed-off-by: vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>

--- practicaljava/pr12.java
+++ practicaljava/pr12.java
@@ -7,7 +7,25 @@ public class pr12 {
 
         for (int i = 0; i <=arr.length; i++) {
             
+            for(int j =i+1; j<arr.length; j++){
+                if (arr[i] > arr[j]) {
+                    
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
             
+        }
+
+        System.out.println("sortes array elements: ");
+
+        for(int i=0; i< arr.length; i++){
+            System.out.print(arr[i]+" ");
         }
     }
 }
+
+OUTPUT:
+sortes array elements: 
+1 2 3 4 5 
