502d7129db60eae3db89149770e67455a89683f7
vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>
2024-10-17 16:52:51 +0530

    Add files via upload
    Signed-off-by: vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>

---
+++ practicaljava/pr18.java
@@ -0,0 +1,12 @@
+package practicaljava;
+class constructors{
+    constructors(){
+        System.out.println("this is a default constructors:");
+    }
+}
+public class pr18 {
+    public static void main(String[] args) {
+     constructors df = new constructors();
+        
+    }
+}
