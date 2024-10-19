1026e45f03190b4a47c0bc40a84584fc2728ea8c
vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>
2024-10-19 10:09:52 +0530

    Add files via upload
    Signed-off-by: vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>

---
+++ practicaljava/pr31.java
@@ -0,0 +1,18 @@
+package practicaljava;
+
+public class pr31 {
+    public static void main(String[] args) {
+        String x = "jayesh";
+
+        String z = x.toLowerCase();
+        String y = x.toUpperCase();
+        System.out.println("lowerCase:" + z);
+        System.out.println("upperCase:" + y);
+    }
+}
+
+/*
+ * OUTPUT:
+ * lowerCase:jayesh
+ * upperCase:JAYESH
+ */
