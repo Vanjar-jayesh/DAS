f933226e532ca267c65db01b25a6c29ca26a1a19
vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>
2024-10-19 10:08:58 +0530

    Add files via upload
    Signed-off-by: vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>

---
+++ practicaljava/pr29/pr029.java
@@ -0,0 +1,20 @@
+package practicaljava.pr29;
+
+public class pr029 {
+    public static void main(String[] args) {
+        jay j = new jay();
+        j.jay1();
+    }
+}
+
+/*
+ * OUTPUT:
+ * enter the Name:
+ * JAYESH
+ * enter the Age:
+ * 20
+ * 
+ * Name:JAYESH
+ * Age:20
+ 
+ */
---
+++ practicaljava/pr29/pr29.java
@@ -0,0 +1,26 @@
+package practicaljava.pr29;
+
+import java.util.Scanner;
+
+class jay{
+
+    public void jay1(){
+        String Name;
+        int Age;
+    
+        System.out.println("enter the Name:");
+        Scanner sc1 = new Scanner(System.in);
+        Name = sc1.nextLine();
+
+        System.out.println("enter the Age:");
+        Scanner sc2 = new Scanner(System.in);
+        Age = sc1.nextInt();
+
+        System.out.println("Name:"+Name);
+        System.out.println("Age:"+Age);
+     
+
+    }
+    
+    
+}
