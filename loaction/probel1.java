21c8b7c3bec1d73b73d976f7ac103d2e1cb7765a
vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>
2024-10-24 17:54:03 +0530

    Add files via upload
    Signed-off-by: vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>

---
+++ loaction/probel1.java
@@ -0,0 +1,40 @@
+
+// package clac;
+
+
+ class calculator{
+
+    public void calculate(int a ,int x){
+        // int c  = a +x;
+        System.out.println("heeloo");
+        System.out.println("your result "+ a+x);
+    }
+    
+}
+
+class sccalculator{
+    
+    public void calculate(int a ,int x){
+        // int c  = a +x;
+        System.out.println("your result "+ a+x);
+    }
+    
+
+}
+ class mycalculator{
+
+    public void calculate(int a ,int x){
+        // int c  = a +x;
+        System.out.println("your result "+ (a+x));
+    }
+    
+}
+public class probel1 {
+    public static void main(String[] args) {
+     calculator oc = new calculator();
+     oc.calculate(10, 20);
+
+        
+    }
+    
+}
