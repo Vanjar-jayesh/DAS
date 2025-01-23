6c2a9eb99708c5cc028cfe7a04c538f9e5afc42a
vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>
2025-01-23 20:30:40 +0530

    Update dowhielop07.java
    Signed-off-by: vanjara_jayesh <155351112+Vanjar-jayesh@users.noreply.github.com>

--- ytnote/dowhielop07.java
+++ ytnote/dowhielop07.java
@@ -8,7 +8,7 @@ public class dowhielop07 {
             System.out.println("Help on: ");
             System.out.println("1 .if");
             System.out.println("2.switch");
-            System.out.println("3.while");
+            System.out.println("3.while");    
             System.out.println("4.do-while");
             System.out.println("5. for\n");
             System.out.println("choose one : ");
